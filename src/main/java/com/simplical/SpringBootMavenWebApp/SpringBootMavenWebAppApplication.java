65a136185ad273c89e6ea93c23a9e48e957a4d2f
student <altafsheikh2007@gmail.com>
2021-07-27 19:08:53 +0000

    My Spring Boot Web App  initial commit

---
+++ src/main/java/com/simplical/SpringBootMavenWebApp/SpringBootMavenWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.simplical.SpringBootMavenWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplical/SpringBootMavenWebApp/SpringBootMavenWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplical.SpringBootMavenWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMavenWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
